75d43b34e0db70a3a76bfd053ed6ce8060428d6a
Viswajith SB <viswajithsaji@gmail.com>
2024-11-13 00:01:45 +0530

    backend work

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express, { request, response } from "express"
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -8,6 +9,16 @@ app.get('/',(request,response)=>{
     return response.status(234).send('welcome to MERN Stack')
 });
 
-app.listen(PORT, ()=>{
-    console.log(`App is listening to port: ${PORT}`);
-});
+
+
+mongoose
+    .connect(mongoDBURL)
+    .then(()=>{
+        console.log("app connected to database");
+        app.listen(PORT, ()=>{
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
